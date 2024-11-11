11cd0d06728ac61d420e445a45e1533445f1b874
Arabian619 <www.arabiandollar152@gmail.com>
2024-11-11 01:16:51 +0000

    Teams Pics Updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,10 +10,10 @@ function Footer() {
                 <p>&#169; 2024 FloodEye. All Rights Reserved.</p>
             </div>
             <div className="socialIcons d-flex justify-content-end">
-                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/flood.eye/profilecard/?igsh=c2lkaTZmNXh6dnR5" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-instagram"></i>
                 </a>
-                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/company/floodeye/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin"></i>
                 </a>
             </div>
--- src/components/team.jsx
+++ src/components/team.jsx
@@ -12,11 +12,12 @@ function Team() {
                 {/* Card 1 */}
                 <div className="card m-5" style={{ width: '25rem', height: '30rem' }}>
                     <div className="card-overlay">
-                        <img src="/images/cempower.png" className="card-img" alt="Founder Adusi Poku Besea" />
+                        <img src="/images/Adusi.png" className="card-img" alt="Founder Adusi Poku Besea" />
                         <div className="card-text">
-                            <h5 className="card-title">ADUSI POKU BESEA</h5><br />
-                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-instagram"></i>
+                            <h5 className="card-title">Adusi Poku Besea</h5>
+                            <p>Founder / CEO</p>
+                            <a href="https://www.linkedin.com/in/adusi-poku-besea/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin"></i>
                             </a>
                         </div>
                     </div>
@@ -24,11 +25,12 @@ function Team() {
               {/* Card 2 */}
               <div className="card m-5" style={{ width: '25rem', height: '30rem' }}>
                     <div className="card-overlay">
-                        <img src="/images/cempower.png" className="card-img" alt="Founder Adusi Poku Besea" />
+                        <img src="/images/Godfrey.png" className="card-img" alt="Godfrey Nyarko" />
                         <div className="card-text">
-                            <h5 className="card-title">ADUSI POKU BESEA</h5><br />
-                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-instagram"></i>
+                            <h5 className="card-title">Godfrey Nyarko</h5>
+                            <p>Co-Founder & Software Engineer</p>
+                            <a href="https://www.linkedin.com/in/godfrey-nyarko/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin"></i>
                             </a>
                         </div>
                     </div>
@@ -36,27 +38,28 @@ function Team() {
               {/* Card 3 */}
               <div className="card m-5" style={{ width: '25rem', height: '30rem' }}>
                     <div className="card-overlay">
-                        <img src="/images/cempower.png" className="card-img" alt="Founder Adusi Poku Besea" />
+                        <img src="/images/Emma.png" className="card-img" alt="Emmanuel Akomeah" />
                         <div className="card-text">
-                            <h5 className="card-title">ADUSI POKU BESEA</h5><br />
-                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-instagram"></i>
+                            <h5 className="card-title">Emmanuel Akomeah</h5>
+                            <p>Co-Founder & Systems Manager</p>
+                            <a href="https://www.linkedin.com/in/emmanuel-akomeah-2a04682b7/" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin"></i>
                             </a>
                         </div>
                     </div>
               </div>
               {/* Card 4 */}
-              <div className="card m-5" style={{ width: '25rem', height: '30rem' }}>
+              {/* <div className="card m-5" style={{ width: '25rem', height: '30rem' }}>
                     <div className="card-overlay">
                         <img src="/images/cempower.png" className="card-img" alt="Founder Adusi Poku Besea" />
                         <div className="card-text">
-                            <h5 className="card-title">ADUSI POKU BESEA</h5><br />
+                            <h5 className="card-title">Emmanuel Akomeah</h5><br />
                             <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-instagram"></i>
                             </a>
                         </div>
                     </div>
-              </div>
+              </div> */}
               {/* More team cards */}
             </div>
           </div>
