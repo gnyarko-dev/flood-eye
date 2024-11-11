fb55b160a29ef44f2c70f8e7f009cb294f889bb9
Arabian619 <www.arabiandollar152@gmail.com>
2024-11-11 04:17:27 +0000

    motion updated

--- src/components/joinus.jsx
+++ src/components/joinus.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import '../styles/styles.css'
+import React from 'react';
+import '../styles/styles.css';
+import { motion } from 'framer-motion';
+import {fadeIn} from '../variant';
 
 function JoinUs() {
   return (
     <div>
         <section id="part__of__ous">
-            <div className="mal">
+          <motion.div
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{once: true, amount: 0.7}}
+          
+          className="mal">
             <h4>Join Us Now</h4>
             <p>Want to be the first person to hear from us?</p>
             <form action="" method="post">
@@ -13,7 +21,7 @@ function JoinUs() {
                 <input type="email" name="email" placeholder="Enter Email" className="nputemal" required />
                 <input type="submit" value="Submit" className="nputSubmt" />
             </form>
-            </div>
+          </motion.div>
         </section>
     </div>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -42,22 +42,33 @@ function Navbar() {
               <b>FloodEye</b>
             </a>
           </motion.div>
-          <motion.ul
-            variants={fadeIn("right", 0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{once: true, amount: 0.7}}
-
+          <ul
             id="menuList"
             style={{ maxHeight: menuOpen ? '300px' : '0px' }}
             className='navlist'
           >
-            <li><a href="/">Home</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/#meet-minds">About Us</a></li>
-            <li><a href="/blog.html">Blogs</a></li>
-            <li><a href="/contacts">Contact</a></li>
-          </motion.ul>
+            <motion.li
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{once: true, amount: 0.7}}
+
+            ><a href="/">Home</a></motion.li>
+            <motion.li
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{once: true, amount: 0.7}}
+            ><a href="/projects">Projects</a></motion.li>
+            <motion.li 
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{once: true, amount: 0.7}}
+            ><a href="/#meet-minds">About Us</a></motion.li>
+            {/* <motion.li><a href="/blog.html">Blogs</a></motion.li> */}
+            {/* <motion.li><a href="/contacts">Contact</a></motion.li> */}
+          </ul>
           <div className="menu-icon" onClick={toggleMenu}>
             <i className="fa-solid fa-bars"></i>
           </div>
